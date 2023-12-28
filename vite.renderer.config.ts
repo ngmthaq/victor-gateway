85ff33cca3eaa87c5d68d65c8e8dafe1d21de1b6
ngmthaq-bglobalcorp <thangnm@bglobalcorp.com>
2023-12-28 09:07:01 +0700

    Remove sqlite

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   build: {
     rollupOptions: {
-      external: ["better-sqlite3, sqlite3"],
+      external: ["better-sqlite3", "sqlite3"],
     },
   },
   resolve: {
