c20e13982a410658b9384d520d7f0486089e472b
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-01-22 20:50:33 +0700

    Update view structure

--- src/configs/constants/path.const.ts
+++ src/configs/constants/path.const.ts
@@ -5,21 +5,27 @@
  */
 
 import type { RouteRecordRaw } from "vue-router";
-import HomeView from "@/renderer/views/Home/HomeView.vue";
-import ActiveView from "@/renderer/views/Active/ActiveView.vue";
+import ActiveViewVue from "@/renderer/views/Active/ActiveView.vue";
 import LoginViewVue from "@/renderer/views/Login/LoginView.vue";
-
-export const PATH_HOME: RouteRecordRaw = {
-  path: "/",
-  component: HomeView,
-};
+import RequestViewVue from "@/renderer/views/Request/RequestView.vue";
+import ResponseViewVue from "@/renderer/views/Response/ResponseView.vue";
 
 export const PATH_ACTIVE: RouteRecordRaw = {
   path: "/active",
-  component: ActiveView,
+  component: ActiveViewVue,
 };
 
 export const PATH_LOGIN: RouteRecordRaw = {
   path: "/login",
   component: LoginViewVue,
+};
+
+export const PATH_REQUEST: RouteRecordRaw = {
+  path: "/request",
+  component: RequestViewVue,
+};
+
+export const PATH_RESPONSE: RouteRecordRaw = {
+  path: "/response",
+  component: ResponseViewVue,
 };
---
+++ src/renderer/views/Response/ResponseView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BaseLayout from "@/renderer/components/layouts/BaseLayout/BaseLayout.vue";
+</script>
+
+<template>
+  <BaseLayout></BaseLayout>
+</template>
+
+<style scoped lang="scss"></style>
